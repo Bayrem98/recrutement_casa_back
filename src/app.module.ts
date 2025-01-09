6cccd2fbd84b2cbe5d67e890ab1e02b5b5abb41c
bayrem98 <bayremsayeh98@gmail.com>
2025-01-09 01:05:32 +0100

    many updated for project dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,20 @@ import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
 import { UploadModule } from './upload/upload.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/recrutement_casa'),
+    ConfigModule.forRoot(),
+    // MongooseModule.forRoot(`${process.env.MONGODB_URI}`),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.gjbdf3j.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`,
+    ),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..'),
+    }),
     UsersModule,
     AdminModule,
     AuthModule,
